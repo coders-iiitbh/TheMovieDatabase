6802a83f3d9cba095a77cfc461648231fd655e71
Aditya Prakash <adityaprakash.bobby@gmail.com>
2018-10-24 04:48:34 +0530

    full-text search added

--- flaskapp.py
+++ flaskapp.py
@@ -1,43 +1,53 @@
 from flask import render_template, url_for, flash, request
 from flask import Flask, redirect
 from forms import SearchForm
-from flask_sqlalchemy import SQLAlchemy
-from flask_whooshalchemy import whoosh_index
+from flask_sqlalchemy import SQLAlchemy, BaseQuery
+from sqlalchemy_searchable import SearchQueryMixin
+from sqlalchemy_utils.types import TSVectorType
+from sqlalchemy_searchable import make_searchable, search
 
 # instantiate the application
 app = Flask(__name__)
 
 # configurations of the flask application
 app.config['SECRET_KEY'] = 'okaycool'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:adi@localhost/sample_db'
-#app.config['SQLALCHEMY_TRACCK_MODIFICATIONS'] = True-
-app.config['WHOOSH_BASE'] = 'whoosh'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:adi@localhost/sample_db'
+app.config['SQLALCHEMY_TRACCK_MODIFICATIONS'] = True
 
 # instantiate the database object
 db = SQLAlchemy(app);
+make_searchable(db.metadata)
+
+# query class
+class MovieQuery(BaseQuery, SearchQueryMixin):
+    pass
 
 # the realtions/tables in form of classes that will be used in the database
 class Movie(db.Model):
-    __searchable__ = ['name']
+
+    query_class = MovieQuery
+    __tablename__ = 'movie'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-    email = db.Column(db.String(30))
+    name = db.Column(db.Unicode(255))
+    content = db.Column(db.UnicodeText)
+    search_vector = db.Column(TSVectorType('name', 'content'))
 
-    def __init__(self, id, name, email):
-        self.id = id
+    def __init__(self, name, content):
         self.name = name
-        self.email = email
+        self.content = content
 
     def __repr__(self):
         return '<Movie %r>' % self.name
 
-whoosh_index(app, Movie)
+# create the movie table/relation
+db.configure_mappers() # very important
+db.create_all()
+db.session.commit()
 
 @app.route('/home')
 @app.route('/')
 def home():
-    #myMovie = Movie()
     return render_template('home.html')
 
 @app.route('/about')
@@ -53,10 +63,8 @@ def search():
 
 @app.route('/search_results/<query>')
 def search_results(query):
-   results = Movie.query.whoosh_search(query).all()
-   return str(request.form)
-  #res =  db.session.query(Movie).all()
-  #print( str(res));
+    results = Movie.query.search(query).limit(5).all()
+    return render_template('search_result.html', results=results)
 
 if __name__ == '__main__' :
     app.run(debug=True, host='0.0.0.0')
