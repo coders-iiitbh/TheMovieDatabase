b5c7040dfba0065ee2e87fcf4dae07f2a5015910
Aditya Prakash <adityaprakash.bobby@gmail.com>
2018-10-29 01:29:50 +0530

    modifications

--- flaskapp.py
+++ flaskapp.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, request
+from flask import render_template, url_for, flash, request, g
 from flask import Flask, redirect
 from forms import SearchForm
 from flask_sqlalchemy import SQLAlchemy, BaseQuery
@@ -28,17 +28,36 @@ class Movie(db.Model):
     query_class = MovieQuery
     __tablename__ = 'movie'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode(255))
-    content = db.Column(db.UnicodeText)
-    search_vector = db.Column(TSVectorType('name', 'content'))
+    rank = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Unicode(255))
+    genre = db.Column(db.UnicodeText)
+    description = db.Column(db.UnicodeText)
+    director = db.Column(db.Unicode(255))
+    actors = db.Column(db.UnicodeText)
+    year = db.Column(db.Integer)
+    runtime = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    votes = db.Column(db.Integer)
+    revenue = db.Column(db.Float)
+    metascore = db.Column(db.Integer)
 
-    def __init__(self, name, content):
-        self.name = name
-        self.content = content
+    search_vector = db.Column(TSVectorType('title', 'description'))
+
+    def __init__(self, title, genre, description, director, actors, year, runtime, rating, votes, revenue, metascore):
+        self.title = title
+        self.genre = genre
+        self.description = description
+        self.director = director
+        self.actors = actors
+        self.year = year
+        self.runtime = runtime
+        self.rating = rating
+        self.votes = votes
+        self.revenue = revenue
+        self.metascore = metascore
 
     def __repr__(self):
-        return '<Movie %r>' % self.name
+        return '<Movie %r>' % self.title
 
 # create the movie table/relation
 db.configure_mappers() # very important
@@ -48,8 +67,8 @@ db.session.commit()
 @app.route('/home')
 @app.route('/')
 def home():
-    posts = Movie.query.limit(10).all()
-    return render_template('home.html', posts=posts)
+    top = Movie.query.filter(Movie.year >= 2016).all()
+    return render_template('home.html', top=top)
 
 @app.route('/about')
 def about():
@@ -58,14 +77,15 @@ def about():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     form = SearchForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        return redirect((url_for('search_results', query=form.search.data)))
+    if form.validate_on_submit():
+        results = Movie.query.search(form.search.data).limit(5).all()
+        return render_template('searchpage.html', results=results, form=form)
     return render_template('searchpage.html', form=form)
 
-@app.route('/search_results/<query>')
-def search_results(query):
-    results = Movie.query.search(query).limit(5).all()
-    return render_template('search_result.html', results=results)
+# @app.route('/search_results/<query>')
+# def search_results(query):
+#     results = Movie.query.search(query).limit(5).all()
+#     return render_template('search_result.html', results=results)
 
 @app.route('/search/<name>', methods=['GET', 'POST'])
 def movie_landing(name=None):
